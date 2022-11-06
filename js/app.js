1f67daa448bc04b346e5a3c69fa9bbea262eaa4c
aster724 <cajares2@gmail.com>
2022-11-06 16:36:49 -0500

    capitulo de mejora

---
+++ js/app.js
@@ -0,0 +1,9 @@
+mport { valida } from "./validaciones.js";
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", (input) => {
+    valida(input.target);
+  });
+});
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,6 +1,31 @@
-const inputNacimiento = document.querySelector('birth');
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
+    }
+  }
+  
+  const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+  };
 
-inputNacimiento.addEventListener('blur', (evento) =>{
-    console.log(evento.target);
-});
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+    let mensaje = '';
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje = 'debes tener al menos 18 años de edad'
+    }
+    
+    input.setCustomValidity(mensaje);
+}
 
+function mayorDeEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciasFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(), 
+    fecha.getUTCDate()
+    );
+    
+    return diferenciasFechas <= fechaActual;
+}
